58c777c1c34b8d4ca45609209a5f4e5997d2fd7b
mvigneshvikee <vigneshvikee0001@gmail.com>
2025-08-06 13:34:49 +0530

    Color Game Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { MovieList } from "./MovieList";
 import { UserList } from "./UserList";
 import "./styles.css";
@@ -11,10 +12,26 @@ export default function App() {
     <div className="App">
       {/* <MsgList /> */}
       {/* <UserList /> */}
-      <MovieList />
+      {/* <MovieList /> */}
       {/* <Counter /> */}
+      <ColorGame />
     </div>
   );
 }
-
-
+function ColorGame() {
+  const [value, setValue] = useState("orange");
+  const boxColor = {
+    backgroundColor: value,
+  };
+  return (
+    <div>
+      <input
+        style={boxColor}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+        type="text"
+      />
+      <h2>{value}</h2>
+    </div>
+  );
+}
