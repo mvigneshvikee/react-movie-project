ec0165b058da462cb4f22b86d5928feb9c5875d8
mvigneshvikee <vigneshvikee0001@gmail.com>
2025-08-08 18:37:10 +0530

    Moving Presentation files to  Component folder

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,15 +1,14 @@
 // Task - User (Component)
 // Presentation Component - Reusablity ⬆️
-import { Counter } from "./Counter";
+import { Counter } from "./components/Counter";
 export function User({ name, pic }) {
-  
   return (
     <div className="user-container">
       <img src={pic} alt={`${name}'s profile pic`} />
       <p>
         Hello, <span>{name} </span>🎉🎉
       </p>
-       <Counter/>
+      <Counter />
     </div>
   );
 }
