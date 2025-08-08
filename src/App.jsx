e697d202ab8d58a3746f6dfb3fd90637b9eb4054
mvigneshvikee <vigneshvikee0001@gmail.com>
2025-08-08 18:33:53 +0530

    Move Movie Details Component to new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { MovieList } from "./MovieList";
-import { UserList } from "./UserList";
+import { Link, Navigate, Route, Routes } from "react-router";
+import { UserList } from "./pages/UserList";
+import { ColorGame } from "./pages/ColorGame";
+import { Home } from "./pages/Home";
+import { MovieDetails } from "./pages/MovieDetails";
+import { MovieList } from "./pages/MovieList";
 import "./styles.css";
-import { ColorGame } from "./ColorGame";
-import { Route, Routes, Link, useParams, useNavigate } from "react-router";
-import { useEffect, useState } from "react";
-import { Navigate } from "react-router";
-import { Home } from "./Home";
 
 // Component = Logic + UI
 export default function App() {
@@ -27,54 +26,6 @@ export default function App() {
         {/* <Route path="addmovie" element={<AddMovies />} /> */}
         <Route path="movies/:id" element={<MovieDetails />} />
       </Routes>
-    </div>
-  );
-}
-
-function MovieDetails() {
-  const { id } = useParams();
-
-  const [movie, setMovie] = useState([]);
-  // console.log(movies);
-  async function getMovies() {
-    const response = await fetch(
-      // `https://68959014039a1a2b288f7c3b.mockapi.io/movies/${id}`
-      "https://68959014039a1a2b288f7c3b.mockapi.io/movies/" + id
-    );
-    const data = await response.json();
-    setMovie(data);
-  }
-  useEffect(() => {
-    getMovies();
-  }, []);
-  const navigate = useNavigate();
-  return (
-    <div
-      style={{
-        marginTop: "20px",
-      }}
-      className="movie-detail-container"
-    >
-      <iframe
-        width="100%"
-        height="650"
-        src={movie.trailer}
-        title="AVENGERS: DOOMSDAY (2026) – FIRST TRAILER | Robert Downey Jr as Doctor Doom | Marvel Comics"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin"
-        allowfullscreen
-      ></iframe>
-      {/* <img src={movie.poster} alt={movie.name} className="movie-poster" /> */}
-      <div className="movie-detail-content-container">
-        <div className="movie-specs">
-          <h2 className="movie-name">{movie.name}</h2>
-          <p className="movie-rating">⭐ {movie.rating}</p>
-        </div>
-
-        <p className="movie-summary">{movie.summary}</p>
-        <button onClick={() => navigate("/movieList")}>Back</button>
-      </div>
     </div>
   );
 }
