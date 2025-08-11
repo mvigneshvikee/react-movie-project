710feee0fe42680873aea597a0eb67ae565294ee
mvigneshvikee <vigneshvikee0001@gmail.com>
2025-08-11 19:15:00 +0530

    Using Material Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import { Link, Navigate, Route, Routes } from "react-router";
-import { UserList } from "./pages/UserList";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Navigate, Route, Routes, useNavigate } from "react-router";
+import { AddMovie } from "./pages/AddMovie";
 import { ColorGame } from "./pages/ColorGame";
 import { Home } from "./pages/Home";
 import { MovieDetails } from "./pages/MovieDetails";
 import { MovieList } from "./pages/MovieList";
-import { AddMovie } from "./pages/AddMovie";
 import { MovieUpdate } from "./pages/MovieUpdate";
+import { UserList } from "./pages/UserList";
 import "./styles.css";
-import { Navbar } from "./Navbar";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import AppBar from "@mui/material/AppBar";
+import Button from "@mui/material/Button";
+import Toolbar from "@mui/material/Toolbar";
 
 // Component = Logic + UI
 export default function App() {
+  const navigate = useNavigate();
   // Logic Starts
   // Logic End
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" onClick={() => navigate("/")}>
+            Home
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/movieList")}>
+            Movies
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/movies/new")}>
+            Add Movie
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/colorgame")}>
+            Color Game
+          </Button>
+          <Button color="inherit" onClick={() => navigate("/userlist")}>
+            Users
+          </Button>
+        </Toolbar>
+      </AppBar>
 
       <Routes>
         <Route path="/" element={<Home />} />
