fd018ccaac5d04fa887c83afa6fd7b0d475771bc
mvigneshvikee <vigneshvikee0001@gmail.com>
2025-08-11 14:32:09 +0530

    Adding Badge Component in Movie Counter

--- src/components/MovieCounter.jsx
+++ src/components/MovieCounter.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Badge from "@mui/material/Badge";
 
 export function MovieCounter() {
   const [like, setLike] = useState(0);
@@ -6,17 +7,23 @@ export function MovieCounter() {
   return (
     <div className="movieCounter-container">
       <div className="buttons">
-        <button onClick={() => setLike(like + 1)}>
-          <i className="fa-solid fa-thumbs-up" style={{ color: "#3e51b5" }}></i>
-          {like}
-        </button>
-        <button onClick={() => setDisLike(disLike + 1)}>
-          <i
-            className="fa-solid fa-thumbs-down fa-flip-horizontal"
-            style={{ color: "#e92064" }}
-          ></i>
-          {disLike}
-        </button>
+        <Badge badgeContent={like} color="primary">
+          <button onClick={() => setLike(like + 1)}>
+            <i
+              className="fa-solid fa-thumbs-up"
+              style={{ color: "#3e51b5" }}
+            ></i>
+          </button>
+        </Badge>
+
+        <Badge badgeContent={disLike} color="error">
+          <button onClick={() => setDisLike(disLike + 1)}>
+            <i
+              className="fa-solid fa-thumbs-down fa-flip-horizontal"
+              style={{ color: "#e92064" }}
+            ></i>
+          </button>
+        </Badge>
       </div>
     </div>
   );
